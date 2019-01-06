ea19f6808f211a42c354b4e9fa2c17eef16c90d0
zzl <1871002454@qq.com>
2019-01-06 20:44:33 +0800

    第一组骨架

---
+++ One_hzp/src/orm/Tzhishi.java
@@ -0,0 +1,63 @@
+package orm;
+
+public class Tzhishi
+{
+	private int id;
+	private String biaoti;
+	private String neirong;
+	private String fujian;
+	
+	private String fujianYuanshiming;
+	private String shijian;
+	
+	
+	public String getBiaoti()
+	{
+		return biaoti;
+	}
+	public void setBiaoti(String biaoti)
+	{
+		this.biaoti = biaoti;
+	}
+	public String getFujian()
+	{
+		return fujian;
+	}
+	public void setFujian(String fujian)
+	{
+		this.fujian = fujian;
+	}
+	public String getFujianYuanshiming()
+	{
+		return fujianYuanshiming;
+	}
+	public void setFujianYuanshiming(String fujianYuanshiming)
+	{
+		this.fujianYuanshiming = fujianYuanshiming;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public String getNeirong()
+	{
+		return neirong;
+	}
+	public void setNeirong(String neirong)
+	{
+		this.neirong = neirong;
+	}
+	public String getShijian()
+	{
+		return shijian;
+	}
+	public void setShijian(String shijian)
+	{
+		this.shijian = shijian;
+	}
+	
+}
